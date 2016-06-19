68fee634196cf3cab507adc3dd267692b8cf6b6c
John Mazzucco <John.Mazzucco@gmail.com>
2016-06-19 08:37:22 -0400

    Create routes for Home and About

---
+++ app/app.js
@@ -0,0 +1,21 @@
+// app.js
+var app = angular.module('routerApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit
+        });
+
+});
