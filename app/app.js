1ddebb05c811178ee660c73fa41f4904f0a38271
John Mazzucco <John.Mazzucco@gmail.com>
2016-06-19 09:13:20 -0400

    Add nested views for Home route

--- app/app.js
+++ app/app.js
@@ -13,6 +13,21 @@ app.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: 'partials/partial-home.html'
         })
 
+       	// nested list with custom controller
+		    .state('home.list', {
+		        url: '/list',
+		        templateUrl: 'partials/partial-home-list.html',
+		        controller: function($scope) {
+		            $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+		        }
+		    })
+
+		    // nested list with just some random string data
+		    .state('home.paragraph', {
+		        url: '/paragraph',
+		        template: 'I could sure use a drink right now.'
+		    })
+
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
             // we'll get to this in a bit
